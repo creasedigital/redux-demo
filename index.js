78ae4f37c0d78f270fc054059677d66f543bb2ba
creasedigital <ojorechris@gmail.com>
2022-05-01 17:49:31 +0100

    3 principles of redux

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("from index.js");
+// First Principle of redux - the state is a global object
+
+// Second Principle of redux - the state can only be changed by dispatching an action e.g. BUY_CAKE
+
+// Third Principle of redux - the state can only be transformed by an action writing a pure Reducers i.e. a function that takes in the previous state and action as an input an returns the new state e.g. (prevState, action) => newState
