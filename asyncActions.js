012815ea6539f5ca9ec5a6616b6cc79a1addebac
creasedigital <ojorechris@gmail.com>
2022-05-02 10:07:40 +0100

    async with thunk

--- asyncActions.js
+++ asyncActions.js
@@ -1,5 +1,9 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+const thunkMiddleware = require("redux-thunk").default;
+const axios = require("axios");
+
 const initialState = {
 	loading: false,
 	users: [],
@@ -16,14 +20,14 @@ const fetchUsersRequest = () => {
 	};
 };
 
-const fetchUsersSuccess = () => {
+const fetchUsersSuccess = (users) => {
 	return {
 		type: FETCH_USERS_SUCCESS,
 		payload: users,
 	};
 };
 
-const fetchUsersFailure = () => {
+const fetchUsersFailure = (error) => {
 	return {
 		type: FETCH_USERS_FAILURE,
 		payload: error,
@@ -51,9 +55,28 @@ const reducer = (state = initialState, action) => {
 				...state,
 				loading: false,
 				users: [],
-				error: action.error,
+				error: action.payload,
 			};
 	}
 };
 
-const store = createStore(reducer);
+const fetchUsers = () => {
+	return function (dispatch) {
+		dispatch(fetchUsersRequest());
+		axios
+			.get("https://jsonplaceholder.typicode.com/users")
+			.then((response) => {
+				const users = response.data.map((user) => user.id);
+				dispatch(fetchUsersSuccess(users));
+			})
+			.catch((error) => {
+				dispatch(fetchUsersFailure(error.message));
+			});
+	};
+};
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+store.subscribe(() => {
+	console.log(store.getState());
+});
+store.dispatch(fetchUsers());
